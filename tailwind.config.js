fd1714ca0a13d650f7fa0b941ce37e9ae3ec7b28
Leonardo <leosud099@gmail.com>
2021-02-23 21:10:47 -0700

    Custom heights

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        "400px": "400px",
+        "500px": "500px",
+        "700px": "700px",
+        "900px": "900px",
+      },
+    },
   },
   variants: {
     extend: {},
